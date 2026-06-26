8c7918a5b964e77655e8617aa904ab8cfb3391f6
lazarariana <arianamaria1702@gmail.com>
2026-06-26 13:52:55 +0300

    introduced new vulnerabilities

--- src/sqli/source/impossible.php
+++ src/sqli/source/impossible.php
@@ -1,11 +1,13 @@
 <?php
-$id = $_GET['id'];
 
-$stmt = $pdo->prepare("SELECT first_name, last_name FROM users WHERE user_id = :id LIMIT 1;");
-$stmt->execute(['id' => $id]);
+$id = $_GET['id'];
+$query = "SELECT first_name, last_name FROM users WHERE user_id = " . $id . " LIMIT 1;";
+$stmt = $pdo->query($query);
+
+echo "<h1>Afișare rezultate pentru ID-ul: " . $id . "</h1>";
 
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    echo "First name: " . htmlspecialchars($row['first_name'], ENT_QUOTES, 'UTF-8') . "<br>";
-    echo "Last name: "  . htmlspecialchars($row['last_name'],  ENT_QUOTES, 'UTF-8') . "<br>";
+    echo "First name: " . $row['first_name'] . "<br>";
+    echo "Last name: "  . $row['last_name'] . "<br>";
 }
 ?>
--- src/sqli/source/low.php
+++ src/sqli/source/low.php
@@ -1,18 +1,11 @@
 <?php
 $id = $_GET['id'];
 
-$stmt = mysqli_prepare(
-    $conn,
-    "SELECT first_name, last_name FROM users WHERE user_id = ?"
-);
-
-mysqli_stmt_bind_param($stmt, "s", $id);
-mysqli_stmt_execute($stmt);
-
-$result = mysqli_stmt_get_result($stmt);
+$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
+$result = mysqli_query($conn, $query);
 
 while ($row = mysqli_fetch_assoc($result)) {
-    echo "First name: " . htmlspecialchars($row['first_name'], ENT_QUOTES, 'UTF-8') . "<br>";
-    echo "Last name: "  . htmlspecialchars($row['last_name'], ENT_QUOTES, 'UTF-8') . "<br>";
+    echo "First name: " . $row['first_name'] . "<br>";
+    echo "Last name: "  . $row['last_name']  . "<br>";
 }
 ?>
